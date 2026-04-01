676f12ca58952e24f8ed9ed0b7f000f3ebae0ebe
Dhaman Kakke <kakkedhamankumar@gmail.com>
2026-03-31 19:18:07 -0500

    query changes

--- Scripts/Querysql.sql
+++ Scripts/Querysql.sql
@@ -1,6 +1,6 @@
 Select 
 	COUNT(*)
-	,DATE_FORMAT(TRANSACTION_DATE,'%Y-%m-01') Mnth
+	-- ,DATE_FORMAT(TRANSACTION_DATE,'%Y-%m-01') Mnth
 	,t.Card_id
 	,c.LAST_4 
 	,b.BANK_NAME 
@@ -9,12 +9,29 @@ join CARDS c
 	on c.CARD_ID = t.CARD_ID 
 join BANKS b 
 	on b.BANK_ID = c.BANK_ID 
-group by DATE_FORMAT(TRANSACTION_DATE,'%Y-%m-01') 
-	,t.Card_id
+group by -- DATE_FORMAT(TRANSACTION_DATE,'%Y-%m-01') 
+	t.Card_id
 	,c.LAST_4 
 	,b.BANK_NAME 
 order by t.Card_id
 	,c.LAST_4 
 	,b.BANK_NAME 
-	,DATE_FORMAT(TRANSACTION_DATE,'%Y-%m-01') 
+	-- ,DATE_FORMAT(TRANSACTION_DATE,'%Y-%m-01') 
+	;
+
+
+	Select 
+	COUNT(*)
+	-- ,DATE_FORMAT(TRANSACTION_DATE,'%Y-%m-01') Mnth
+	,t.Card_id
+	,b.BANK_NAME 
+from TRANSACTION_REPORTING t 
+join BANKS b 
+	on b.BANK_ID = t.BANK_ID 
+group by -- DATE_FORMAT(TRANSACTION_DATE,'%Y-%m-01') 
+	t.Card_id
+	,b.BANK_NAME 
+order by t.Card_id
+	,b.BANK_NAME 
+	-- ,DATE_FORMAT(TRANSACTION_DATE,'%Y-%m-01') 
 	;
